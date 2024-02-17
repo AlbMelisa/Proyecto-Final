55d071a7c4a5c588b96705667e8a8a4e1462687b
tomitas1x <rafa111x_ortega@hotmail.com>
2024-02-17 18:38:23 -0300

    Cambios admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import Grilla from '../../components/Grilla/Grilla'
 
 const Admin = ({setUser}) => {
-  return (
-    <Grilla/>
-  )
-}
+    return (
+      <div className="container">
+        <h1>BIENVENIDOS AL PANEL DE ADMINISTRACION</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">USER</th>
+              <th scope="col">MAIL</th>
+              <th scope="col">ROLE</th>
+              <th scope="col">UPGRADE</th>
+            </tr>
+          </thead>
+          <tbody id="tableBody">
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
 export default Admin
