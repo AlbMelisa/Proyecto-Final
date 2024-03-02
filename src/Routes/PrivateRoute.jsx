e7672cfca994fcb086c324b432553e805a2de3bf
Camila Brahim <brahimcamilaayelen@gmail.com>
2024-03-02 15:06:51 -0300

    actalizaciones pagina sobre nosotros(rutas)

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -11,6 +11,7 @@ import Grilla from '../components/Grilla/Grilla'
 import CrudPlanes from '../pages/CrudPlanes/CrudPlanes'
 import CrudUsuarios from '../pages/CrudUsuarios/CrudUsuarios'
 import CrudClases from '../pages/CrudClases/CrudClases'
+import SobreNosotros from '../pages/SobreNosotros/SobreNosotros'
 
 const PrivateRoute = ({setUser, isLogged,userInfo}) => {
   return (
@@ -21,15 +22,17 @@ const PrivateRoute = ({setUser, isLogged,userInfo}) => {
       {userInfo && userInfo.role === 'admin' && (
         <>
           <Route path='/crud/Planes' element={<CrudClases setUSer={setUser} nombre="planes"/>} />
-          <Route path='/crud/Usuarios' element={<CrudClases setUSer={setUser} nombre="usuarios"/>}/>
+         <Route path='/crud/Usuarios' element={<CrudClases setUSer={setUser} nombre="usuarios"/>}/>
           <Route path='/crud/Clases' element={<CrudClases setUSer={setUser} nombre="clases"/>}/>
         </>
-      )}
+      )}  
+      
       <Route path='/Plan/full' element={<Plan isLogged={isLogged} nombre="full"/>} />
       <Route path='/Plan/musculacion' element={<Plan isLogged={isLogged} nombre="musculacion" />} />
       <Route path='/Plan/clases' element={<Plan isLogged={isLogged} nombre="clases"/>} />
       <Route path='/Registro' element={<Registro/>}/>
       <Route path='/Contacto' element={<Contacto/>}/>
+      <Route path='/SobreNosotros' element={<SobreNosotros/>}/>
       <Route path='/Error404' element={<Error404/>}/>
       <Route exact path='/Error404' element={<Error404 />} />
       <Route path='*' element={<Navigate to='/' />} />
